4a7e3eda3b20ef3867b863c2cd482b147a3db53d
Siyi Li <slii0048@student.monash.edu>
2021-10-13 14:47:01 +1100

    add all

---
+++ R/key_crop_yields.R
@@ -0,0 +1,21 @@
+
+#' Title
+#'
+#'More details about the data
+#'
+#'
+#' @section Data contents:
+#'
+#' @format A data frame with 53940 rows ans 10 variables;
+#' \describe{
+#'  \item{price}{price, in US dollar}
+#'  \item{carat}{weight of dimond, in carats}
+#'  ...
+#'
+#'}
+#'
+#' @source where you get the data
+#' @examples
+#' print(key_crop_yields)
+#'
+"key_crop_yields"
